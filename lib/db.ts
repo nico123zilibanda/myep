0ef9fd28094d4176905edbc1a17c7ddc2417c487
nico123zilibanda <145332137+nico123zilibanda@users.noreply.github.com>
2026-01-06 05:11:17 +0300

    update changes

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,12 @@
-import pkg from "pg";
-const { Pool } = pkg;
+import { Pool } from "pg";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // muhimu kwa Supabase
+  },
 });
 
-export const query = (text: string, params?: any[]) => pool.query(text, params);
+export const query = (text: string, params?: any[]) => {
+  return pool.query(text, params);
+};
